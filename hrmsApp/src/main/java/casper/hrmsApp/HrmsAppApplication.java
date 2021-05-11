8918971652fd8954f0dd53bf9623ceafd71b5825
ibrhmusta <ibrhmusta99@gmail.com>
2021-05-11 17:23:55 +0300

    Katmanlar oluşturuldu...

---
+++ hrmsApp/src/main/java/casper/hrmsApp/HrmsAppApplication.java
@@ -0,0 +1,13 @@
+package casper.hrmsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsAppApplication.class, args);
+	}
+
+}
