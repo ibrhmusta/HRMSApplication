ced78f51402453784668e29d7583221a2ef90651
ibrhmusta <ibrhmusta99@gmail.com>
2021-05-24 23:50:29 +0300

    Results Added

---
+++ hrmsApp/src/main/java/casper/hrmsApp/core/utilities/results/SuccessResult.java
@@ -0,0 +1,10 @@
+package casper.hrmsApp.core.utilities.results;
+
+public class SuccessResult extends Result{
+    public SuccessResult(){
+        super(true);
+    }
+    public SuccessResult(String message){
+        super(true,message);
+    }
+}
